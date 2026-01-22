bc54806fcc701152193c48b0646870679b54b356
Alecu Catalin <alecucatalindaniel@gmail.com>
2026-01-22 21:07:20 +0200

    login and register
    Login and register forms added

--- licenta/CliniCata/clinica/urls.py
+++ licenta/CliniCata/clinica/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from clinica import views
+from .views import RegisterView, MyLoginView
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', MyLoginView.as_view(), name='login'),
 ]
--- licenta/CliniCata/clinica/views.py
+++ licenta/CliniCata/clinica/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+
+class MyLoginView(LoginView):
+    template_name = 'login.html'
+
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('home')
