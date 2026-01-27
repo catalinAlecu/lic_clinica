1d568de5a1786b32f834dfb76409e5b0fe71eb90
Alecu Catalin <alecucatalindaniel@gmail.com>
2026-01-27 20:55:15 +0200

    fixed a bug

--- licenta/CliniCata/clinica/views.py
+++ licenta/CliniCata/clinica/views.py
@@ -26,6 +26,11 @@ class RegisterView(CreateView):
     template_name = 'register.html'
     success_url = reverse_lazy('login')
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super().dispatch(request, *args, **kwargs)
+
     def form_valid(self, form):
         response = super().form_valid(form)
         user = self.object 
