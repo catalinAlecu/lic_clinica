f71d453e76fc555169f84fee29a11cf5fb325f51
Alecu Catalin <alecucatalindaniel@gmail.com>
2026-05-04 20:57:45 +0300

    vercel entrypoint

--- licenta/CliniCata/api/index.py
+++ licenta/CliniCata/api/index.py
@@ -1,10 +1,9 @@
 import os
 import sys
+from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CliniCata.settings')
-
-from django.core.wsgi import get_wsgi_application
 
 app = get_wsgi_application()
