78ee878a3668ac9e9ac51349724873f1588a258e
Alecu Catalin <alecucatalindaniel@gmail.com>
2026-05-05 19:29:16 +0300

    domain added

--- licenta/CliniCata/CliniCata/settings.py
+++ licenta/CliniCata/CliniCata/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "licclinica-production.up.railway.app"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "licclinica-production.up.railway.app", "clinicata.site"]
 
 
 # Application definition
