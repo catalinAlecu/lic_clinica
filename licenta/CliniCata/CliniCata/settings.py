4b49ffc136bf06ad9dd08158395f2a78b45a7cbb
Alecu Catalin <alecucatalindaniel@gmail.com>
2026-05-06 11:41:22 +0300

    fixed static folder

--- licenta/CliniCata/CliniCata/settings.py
+++ licenta/CliniCata/CliniCata/settings.py
@@ -20,7 +20,11 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 env_path = BASE_DIR / 'clinic.env'
 load_dotenv(env_path)
 
