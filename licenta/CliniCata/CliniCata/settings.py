b0f7dd874d2b19b5646864294ac43cf06c71eb0f
Alecu Catalin <alecucatalindaniel@gmail.com>
2026-05-06 11:28:13 +0300

    staticfiles fix

--- licenta/CliniCata/CliniCata/settings.py
+++ licenta/CliniCata/CliniCata/settings.py
@@ -126,7 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Aici adună Railway toate fișierele pentru producție
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = '/'
 
