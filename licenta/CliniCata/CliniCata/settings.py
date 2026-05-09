96431a794851062a201cc4cb4322c67134a9df3b
Alecu Catalin <alecucatalindaniel@gmail.com>
2026-05-09 21:00:49 +0300

    static fix

--- licenta/CliniCata/CliniCata/settings.py
+++ licenta/CliniCata/CliniCata/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'clinica', 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 env_path = BASE_DIR / 'clinic.env'
